1b8e83778ac752cce1d577bf70f83217a0e6fb02
Niyati <niyatidesai1122@gmail.com>
2025-04-22 21:56:08 -0700

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Niyati Desai",
+    author_email="niyatidesai1122@gmail.com",
+    packages=find_packages()
+)
